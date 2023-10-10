7acac5af01f6d3451c946f7569c4a245759e607d
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-10-10 14:54:08 +0530

    Update player_details.js

--- player_details.js
+++ player_details.js
@@ -4,13 +4,11 @@ const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
 const PLAYER_SHEET_NAME = 'snookerplus';
 const FRAMES_SHEET_NAME = 'Frames';
 
-// Initialize Google Sheets API client
 function initClient() {
     gapi.client.init({
         apiKey: API_KEY,
         discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(function () {
-        // Retrieve player name from URL parameter
         const urlParams = new URLSearchParams(window.location.search);
         const playerName = urlParams.get('player');
         
@@ -23,24 +21,55 @@ function initClient() {
     });
 }
 
-// Function to fetch and display player info
 function fetchPlayerInfo(playerName) {
-    // ... (Code to fetch player info from Google Sheets)
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: SHEET_ID,
+        range: PLAYER_SHEET_NAME,
+    }).then(function (response) {
+        const values = response.result.values;
+        const playerInfo = values.find(row => row[2] === playerName); // Assuming name is in column C
+        if (playerInfo) {
+            displayPlayerInfo(playerInfo);
+        } else {
+            console.log('Player not found.');
+        }
+    }, function (response) {
+        console.error('Error fetching player data:', response.result.error.message);
+    });
 }
 
-// Function to fetch and display frames info
 function fetchFramesInfo(playerName) {
-    // ... (Code to fetch frames info from Google Sheets)
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: SHEET_ID,
+        range: FRAMES_SHEET_NAME,
+    }).then(function (response) {
+        const values = response.result.values;
+        const framesData = values.filter(row => 
+            [row[12], row[13], row[14], row[15], row[16], row[17]].includes(playerName)
+        );
+        if (framesData.length > 0) {
+            displayFramesInfo(framesData);
+        } else {
+            console.log('No frames found for player.');
+        }
+    }, function (response) {
+        console.error('Error fetching frames data:', response.result.error.message);
+    });
 }
 
-// Display functions for player and frame info
 function displayPlayerInfo(playerInfo) {
-    // ... (Code to display player info on the webpage)
+    document.getElementById('playerName').innerText = playerInfo[2]; // Assuming name is in column C
+    document.getElementById('totalMoney').innerText = `Total Money: ${playerInfo[6]}`; // Assuming total money is in column G
+    // Add more player info display logic as needed
 }
 
 function displayFramesInfo(framesData) {
-    // ... (Code to display frame info on the webpage)
+    const framesContainer = document.getElementById('framesInfo');
+    framesData.forEach(frame => {
+        const frameElement = document.createElement('div');
+        frameElement.innerText = `Date: ${frame[2]}, Duration: ${frame[3]}, Winner: ${frame[5]}`; // Adjust column indices as needed
+        framesContainer.appendChild(frameElement);
+    });
 }
 
-// Call the initClient function to start fetching data
-initClient();
+gapi.load('client', initClient);
