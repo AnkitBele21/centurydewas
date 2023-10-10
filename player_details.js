6f87e06c3cb95ffa384dc574b98c06d78e6b516f
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-10-10 14:11:59 +0530

    Update player_details.js

--- player_details.js
+++ player_details.js
@@ -1,5 +1,5 @@
 // Your API Key and Sheet ID
-const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
+const API_KEY = 'YOUR_API_KEY';
 const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
 const PLAYER_SHEET_NAME = 'Snookerplus*';
 const FRAMES_SHEET_NAME = 'Frames';
@@ -10,63 +10,30 @@ function initClient() {
         apiKey: API_KEY,
         discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(function () {
-        const urlParams = new URLSearchParams(window.location.search);
-        const playerName = decodeURIComponent(urlParams.get('player'));
-        if (playerName) {
-            fetchPlayerInfo(playerName);
-            fetchFramesInfo(playerName);
-        } else {
-            console.error('Player name is missing in URL parameters.');
-        }
+        // Example usage (replace 'John Doe' with the desired player name)
+        const playerName = 'John Doe'; // TODO: Get this from user input or another source
+        fetchPlayerInfo(playerName);
+        fetchFramesInfo(playerName);
     });
 }
 
 // Function to fetch and display player info
 function fetchPlayerInfo(playerName) {
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: SHEET_ID,
-        range: PLAYER_SHEET_NAME,
-    }).then(function (response) {
-        const values = response.result.values;
-        if (values && values.length > 0) {
-            const playerInfo = values.find(row => row[2] === playerName);
-            if (playerInfo) {
-                console.log('Player Info:', playerInfo);
-                // TODO: Display player info on the webpage
-            } else {
-                console.log('Player not found.');
-            }
-        } else {
-            console.log('No data found.');
-        }
-    }, function (response) {
-        console.error('Error fetching data:', response.result.error.message);
-    });
+    // ... (same as in your previous code)
 }
 
 // Function to fetch and display frames info
 function fetchFramesInfo(playerName) {
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: SHEET_ID,
-        range: FRAMES_SHEET_NAME,
-    }).then(function (response) {
-        const values = response.result.values;
-        if (values && values.length > 0) {
-            const framesData = values.filter(row =>
-                [row[12], row[13], row[14], row[15], row[16], row[17]].includes(playerName)
-            );
-            if (framesData.length > 0) {
-                console.log('Frames Info:', framesData);
-                // TODO: Display frames info on the webpage
-            } else {
-                console.log('Player not found in frames.');
-            }
-        } else {
-            console.log('No data found.');
-        }
-    }, function (response) {
-        console.error('Error fetching data:', response.result.error.message);
-    });
+    // ... (same as in your previous code)
+}
+
+// Display functions from the previous message
+function displayPlayerInfo(playerInfo) {
+    // ... (same as in the previous message)
+}
+
+function displayFramesInfo(framesData) {
+    // ... (same as in the previous message)
 }
 
 // Call the initClient function to start fetching data
