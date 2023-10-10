82df11cc57a55f586b0eb9207f123068b97f1870
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-10-10 13:46:12 +0530

    Update player_details.js

--- player_details.js
+++ player_details.js
@@ -1,37 +1,8 @@
 // Your API Key and Sheet ID
 const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
 const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
-const PLAYER_SHEET_NAME = 'snookerplus';
+const PLAYER_SHEET_NAME = 'Snookerplus*';
 const FRAMES_SHEET_NAME = 'Frames';
-
-// Function to fetch and display player info
-function fetchPlayerInfo(playerName) {
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: SHEET_ID,
-        range: PLAYER_SHEET_NAME,
-    }).then(function (response) {
-        // Handle player info...
-    }, function (response) {
-        console.error('Error fetching data:', response.result.error.message);
-    });
-}
-
-// Function to fetch and display frames info
-function fetchFramesInfo(playerName) {
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: SHEET_ID,
-        range: FRAMES_SHEET_NAME,
-    }).then(function (response) {
-        // Handle frames info...
-    }, function (response) {
-        console.error('Error fetching data:', response.result.error.message);
-    });
-}
-
-// Ensure the API is loaded before initializing
-window.onload = function() {
-    gapi.load('client', initClient);
-};
 
 // Initialize Google Sheets API client
 function initClient() {
@@ -45,3 +16,39 @@ function initClient() {
         fetchFramesInfo(playerName);
     });
 }
+
+function fetchPlayerInfo(playerName) {
+    // Fetching and displaying player info logic...
+    // [Your existing logic here]
+}
+
+function fetchFramesInfo(playerName) {
+    // Fetching and displaying frames info logic...
+    // [Your existing logic here]
+}
+
+function displayPlayerInfo(playerInfo) {
+    // Displaying player info logic...
+    document.getElementById('playerName').textContent = playerInfo.name;
+    document.getElementById('totalMoney').textContent = `Total Money: ${playerInfo.totalMoney}`;
+    document.getElementById('tableMoney').textContent = `Table Money: ${playerInfo.tableMoney}`;
+    document.getElementById('balance').textContent = `Balance: ${playerInfo.balance}`;
+}
+
+function displayFramesInfo(framesData) {
+    const framesList = document.getElementById('framesList');
+    framesList.innerHTML = '';  // Clear previous data
+
+    if (framesData && framesData.length > 0) {
+        framesData.forEach(frame => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `Date: ${frame[2]}, Duration: ${frame[3]} minutes, Winner: ${frame[5]}`;
+            framesList.appendChild(listItem);
+        });
+    } else {
+        framesList.innerHTML = '<li>No frames data found for this player.</li>';
+    }
+}
+
+// Call the initClient function to start fetching data
+initClient();
