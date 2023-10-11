2055196b518747313016580781f28b784d50a0b6
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-10-11 23:29:10 +0530

    Update datapull.js

--- datapull.js
+++ datapull.js
@@ -3,6 +3,9 @@ const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
 const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
 const SHEET_NAME = 'Rank';
 
+// List of previous champions
+const champions = ["Arpit", "Saurav Johari"];
+
 // Load the Google Sheets API
 gapi.load('client', initClient);
 
@@ -31,114 +34,22 @@ function createPlayerCard(player) {
     playerName.className = 'player-name';
     playerName.textContent = `${rank}. ${name}`;
 
-    const playerCoins = document.createElement('span');
-    playerCoins.className = 'player-coins';
-    playerCoins.textContent = `S+ Coins: ${coins}`;
-
-    const progressBar = document.createElement('div');
-    progressBar.className = 'progress-bar';
-
-    let progressBarColor = '#F44336'; // Default: Red
-
-    if (coins >= 11 && coins <= 30) {
-        progressBarColor = '#FFEB3B'; // Yellow
-    } else if (coins >= 31 && coins <= 60) {
-        progressBarColor = '#4CAF50'; // Green
-    } else if (coins >= 61 && coins <= 100) {
-        progressBarColor = '#795548'; // Brown
-    } else if (coins >= 101 && coins <= 150) {
-        progressBarColor = '#2196F3'; // Blue
-    } else if (coins >= 151 && coins <= 210) {
-        progressBarColor = '#E91E63'; // Pink
-    } else if (coins > 210) {
-        progressBarColor = '#000000'; // Black
+    // Check if player is a champion and add a medal icon
+    if (champions.includes(name)) {
+        const championIcon = document.createElement('img');
+        championIcon.src = 'path_to_medal_icon.png'; // Replace with the path to your medal icon
+        championIcon.alt = 'Champion';
+        championIcon.className = 'champion-icon';
+        playerName.appendChild(championIcon);
     }
 
-    progressBar.style.backgroundColor = progressBarColor;
-
-    const colorMinCoins = [0, 11, 31, 61, 101, 151, 211];
-    const colorMaxCoins = [10, 30, 60, 100, 150, 210, 1000];
-    let progressBarWidth = 0;
-
-    for (let i = 0; i < colorMinCoins.length; i++) {
-        if (coins >= colorMinCoins[i] && coins <= colorMaxCoins[i]) {
-            progressBarWidth = ((coins - colorMinCoins[i]) / (colorMaxCoins[i] - colorMinCoins[i] + 1)) * 100;
-            break;
-        }
-    }
-
-    if ([11, 31, 61, 101, 151, 211].includes(coins)) {
-        progressBarWidth = Math.max(progressBarWidth, 2); // Ensuring at least 2% width
-    }
-
-    progressBar.style.width = `${progressBarWidth}%`;
+    // ... [Rest of the code remains the same]
 
     playerInfo.appendChild(playerName);
-    playerInfo.appendChild(playerCoins);
     playerCard.appendChild(playerInfo);
-    playerCard.appendChild(progressBar);
 
     return playerCard;
 }
 
-// Function to display players
-function displayPlayers(players) {
-    const playerContainer = document.getElementById('playerContainer');
-    playerContainer.innerHTML = '';
-    players.forEach(player => {
-        playerContainer.appendChild(createPlayerCard(player));
-    });
-}
+// ... [Rest of the code remains the same]
 
-// Function to fetch data from Google Sheets
-function fetchSheetData() {
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: SHEET_ID,
-        range: SHEET_NAME,
-    }).then(function(response) {
-        const values = response.result.values;
-        if (values && values.length > 0) {
-            const players = values.map((row, index) => ({
-                rank: index + 1,
-                name: row[1],
-                coins: parseInt(row[2]),
-            }));
-            displayPlayers(players);
-        } else {
-            console.log('No data found.');
-        }
-    }, function(response) {
-        console.error('Error fetching data:', response.result.error.message);
-    });
-}
-
-// Function to search and filter data
-function searchTable() {
-    var input, filter, cards, name, i;
-    input = document.getElementById("searchInput");
-    filter = input.value.toUpperCase();
-    cards = document.getElementsByClassName("player-card");
-    for (i = 0; i < cards.length; i++) {
-        name = cards[i].getElementsByClassName("player-name")[0].textContent;
-        if (name.toUpperCase().indexOf(filter) > -1) {
-            cards[i].style.display = "";
-        } else {
-            cards[i].style.display = "none";
-        }
-    }
-}
-let lastScrollTop = 0;
-const floatingButton = document.getElementById('floatingButton');
-
-window.addEventListener("scroll", function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-        floatingButton.style.opacity = "0";
-    } else {
-        floatingButton.style.opacity = "1";
-    }
-    lastScrollTop = scrollTop;
-});
-
-// Call the initClient function to start fetching data
-initClient();
