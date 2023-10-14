f95c41bb122c8d9c76d76b935de1b620f9077ac2
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-10-15 02:29:19 +0530

    Update club_dashboard.js

--- club_dashboard.js
+++ club_dashboard.js
@@ -8,33 +8,74 @@ async function fetchData(sheetName) {
     return data.values;
 }
 
-function createGraph(data, labels, canvasId) {
+// ... [Previous Code]
+
+function createGraph(data, labels, canvasId, graphTitle) {
     var ctx = document.getElementById(canvasId).getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: labels,
             datasets: [{
-                label: 'Performance',
+                label: graphTitle,
                 data: data,
+                backgroundColor: '#01AB7A',
+                borderColor: '#018a5e',
+                borderWidth: 1,
             }]
         },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false // Hides legend to reduce clutter
+                },
+                tooltip: {
+                    callbacks: {
+                        title: function(context) {
+                            // Customizing tooltip title
+                            return `Details for ${labels[context[0].dataIndex]}`;
+                        }
+                    }
+                }
+            },
+            title: {
+                display: true,
+                text: graphTitle,
+                font: {
+                    size: 18,
+                    weight: 'bold'
+                },
+                color: '#01AB7A'
+            }
+        }
     });
 }
 
 async function createTableWisePerformanceGraph() {
     const data = await fetchData('club');
-    const tables = data.map(row => row[0]);
+    const tables = data.map((row, index) => index + 1); // Simplified labels
     const occupancy = data.map(row => row[1]);
-    createGraph(occupancy, tables, 'tableWisePerformanceChart');
+    const filteredData = occupancy.filter(val => val !== '0'); // Exclude zero values
+    const filteredTables = tables.slice(0, filteredData.length); // Match labels to data length
+    createGraph(filteredData, filteredTables, 'tableWisePerformanceChart', 'Tables Occupied');
 }
 
 async function createDateWisePerformanceGraph() {
     const data = await fetchData('club');
-    const dates = data.map(row => row[7]);
+    const dates = data.map((row, index) => index + 1); // Simplified labels
     const occupancy = data.map(row => row[9]);
-    createGraph(occupancy, dates, 'dateWisePerformanceChart');
+    const filteredData = occupancy.filter(val => val !== '0'); // Exclude zero values
+    const filteredDates = dates.slice(0, filteredData.length); // Match labels to data length
+    createGraph(filteredData, filteredDates, 'dateWisePerformanceChart', 'Club Performance by Date');
 }
+
+// ... [Previous Code]
+
 
 // Initialize graphs on page load
 window.onload = function() {
