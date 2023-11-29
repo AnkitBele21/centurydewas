574dec73483256d521b1f354fd518dca0702a011
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-11-30 00:28:06 +0530

    Update player_details.js

--- player_details.js
+++ player_details.js
@@ -1,7 +1,7 @@
 // player_details.js
 // Your API Key and Sheet ID
 const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
-const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
+const SHEET_ID = '1RmMxuj_taiFoFx9V20xa_l8E74Wg-jaKTMexCfYpCTw';
 const PLAYER_SHEET_NAME = 'snookerplus';
 const FRAMES_SHEET_NAME = 'Frames';
 const RANK_SHEET_NAME = 'Rank';
