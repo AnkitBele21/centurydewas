2a02e818117708e0163d2d2395a0817480347de9
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2024-03-15 12:58:54 +0530

    Create updateactiveframe.js

---
+++ updateactiveframe.js
@@ -0,0 +1,6 @@
+document.getElementById('updateFrameForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here, you would add your logic for fetching the frame number and handling the form data.
+    // For demonstration, we'll just redirect.
+    window.location.href = 'https://ankitbele21.github.io/centurydewas/clubframes';
+});
